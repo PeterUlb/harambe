0bef2a36fa5c2cfad93f86518e909d59fe6a12e1
mschlechtinger <michael.schlechtinger@sap.com>
2016-09-05 16:33:42 +0200

    Sample UI

--- src/main/java/com/harambe/App.java
+++ src/main/java/com/harambe/App.java
@@ -1,11 +1,29 @@
 package com.harambe;
 
-/**
- * Hello world!
- *
- */
-public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    Stage window;
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
     }
+
+
+    public void start(Stage stage) throws Exception {
+        Pane root = (Pane) FXMLLoader.load(getClass().getClassLoader().getResource("scenes/sample.fxml"));
+        Scene scene = new Scene(root, 1280, 800);
+        stage.setTitle("test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
---
+++ src/main/java/com/harambe/gui/SampleController.java
@@ -0,0 +1,8 @@
+package com.harambe.gui;
+
+/**
+ * Created by D062589 on 05.09.2016.
+ */
+public class SampleController {
+
+}
