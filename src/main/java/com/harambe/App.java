5f232575dc080eb1144ec72727e1f8319873542f
Ulbrich <D062592@global.corp.sap>
2016-09-05 14:26:44 +0200

    initial commit

---
+++ src/main/java/com/harambe/App.java
@@ -0,0 +1,13 @@
+package com.harambe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
