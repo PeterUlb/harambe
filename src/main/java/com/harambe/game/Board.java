03f33e9e28d25b6b4f2727a69d3b803cec7d81f0
PeterUlb <ulbrich-p@web.de>
2016-09-12 14:54:20 +0200

    win location for UI

--- src/main/java/com/harambe/game/Board.java
+++ src/main/java/com/harambe/game/Board.java
@@ -204,12 +204,82 @@ public class Board {
         return false;
     }
 
+
+    /**
+     * Win location for the UI
+     * @param playerSymbol the playerSymbol (Board.PlayerX)
+     * @return 2d array of 4-fields (4x 2 integers in form row, column)
+     */
+    public int[][] getWinForUI(char playerSymbol) {
+        // -
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLUMNS - 3; col++) {
+                if (grid[row][col] == playerSymbol) {
+                    if (grid[row][col + 1] == playerSymbol && grid[row][col + 2] == playerSymbol && grid[row][col + 3] == playerSymbol) {
+                        return new int[][] { {row, col}, {row,col + 1}, {row, col + 2}, {row, col + 3}};
+                    }
+                }
+            }
+        }
+
+        // |
+        for (int col = 0; col < COLUMNS; col++) {
+            for (int row = 0; row < ROWS - 3; row++) {
+                if (grid[row][col] == playerSymbol) {
+                    if (grid[row + 1][col] == playerSymbol && grid[row + 2][col] == playerSymbol && grid[row + 3][col] == playerSymbol) {
+                        return new int[][] { {row, col}, {row + 1, col}, {row + 2, col}, {row + 3, col}};
+                    }
+                }
+            }
+        }
+
+        // /
+        for (int row = ROWS - 4; row >= 0; row--) {
+            for (int col = COLUMNS - 4; col >= 0; col--) {
+                int hitCount = 0;
+                int[][] returnValue = new int[4][2];
+                for (int val = 3; val >= 0; val--) {
+                    char symbol = grid[row + val][col - val + 3];
+                    if (symbol == playerSymbol) {
+                        returnValue[val][0] = row + val;
+                        returnValue[val][1] = col - val + 3;
+                        hitCount++;
+                    }
+                }
+                if (hitCount == 4) {
+                    return returnValue;
+                }
+            }
+        }
+
+        // \
+        for (int row = ROWS - 4; row >= 0; row--) {
+            for (int col = COLUMNS - 4; col >= 0; col--) {
+                int hitCount = 0;
+                int[][] returnValue = new int[4][2];
+                for (int val = 3; val >= 0; val--) {
+                    char symbol = grid[row + val][col + val];
+                    if (symbol == playerSymbol) {
+                        returnValue[val][0] = row + val;
+                        returnValue[val][1] = col + val;
+                        hitCount++;
+                    }
+                }
+                if (hitCount == 4) {
+                    return returnValue;
+                }
+            }
+        }
+
+        return null;
+    }
+
     //responds with free columnSpace
     public int[] getFirstAvailableRow() {
         return firstAvailableRow;
     }
 
-    public Boolean isFull(int column) {
+    public boolean isFull(int column) {
         if (getFirstAvailableRow()[column]<0) {
             return true;
         }
